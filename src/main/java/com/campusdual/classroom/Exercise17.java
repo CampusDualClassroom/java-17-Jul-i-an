4dec0d0467c31fda46e35bb4e8b95b60eb07391d
Julian Ramiro Sanchez <julian.ramiro@gmail.com>
2024-09-30 13:58:33 +0200

    Ejercicio completado

--- src/main/java/com/campusdual/classroom/Exercise17.java
+++ src/main/java/com/campusdual/classroom/Exercise17.java
@@ -1,15 +1,32 @@
 package com.campusdual.classroom;
 
+import javax.management.StringValueExp;
+
 public class Exercise17 {
-	public static int[] createEmptyIntArray(int positions) {}
+	public static int[] createEmptyIntArray(int positions) {
+		int[] intArray = new int[positions];
+		return intArray;
+	}
 
-	public static String[] createInitializedStringArray() {}
+	public static String[] createInitializedStringArray() {
+		String[] stringArray = new String[3];
+		for (int i = 0; i < 3; i++){
+			stringArray[i] = String.valueOf((i+1));
+		}
+		return stringArray;
+	}
 
-	public static void populateArray(int[] array, int index, int value) {}
+	public static void populateArray(int[] array, int index, int value) {
+		array[index] = value;
+	}
 
-	public static int arrayLength(int[] array) {}
+	public static int arrayLength(int[] array) {
+		return array.length;
+	}
 
-	public static void showValue(int[] array, int index) {}
+	public static void showValue(int[] array, int index) {
+		System.out.println(array[index]);
+	}
 
 	public static void main(String[] args) {
 		int[] intArray = createEmptyIntArray(3);
